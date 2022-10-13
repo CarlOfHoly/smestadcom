4453c8d8e7199e93f21d2e7b71b7f4a0a108578c
Carl Smestad <carl.smestad@gmail.com>
2022-10-13 18:00:20 +0200

    add iam role for github_actions

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -59,7 +59,7 @@ resource "aws_s3_bucket" "website" {
   force_destroy = true
   website {
     index_document  = "index.html"
-    eerror_document = "index.html"
+    error_document = "index.html"
   }
 
 }
@@ -78,4 +78,35 @@ data "aws_iam_policy_document" "s3_access" {
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.website.id
   policy = data.aws_iam_policy_document.s3_access.json
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${local.name_prefix}-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_assume.json
+  tags               = local.tags
+}
+
+data "aws_iam_policy_document" "github_assume" {
+  statement {
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      # TODO: Replace <username> and <repository> with your GitHub username and repository name
+      values   = ["repo:/CarlOfHoly/smestad.com:*"]
+    }
+  }
 }
