6ae82f9ee4628a9d576b6970a4397c3660681cae
Carl Smestad <carl.smestad@gmail.com>
2022-10-13 18:53:13 +0200

    change url for sub in policy

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -106,7 +106,7 @@ data "aws_iam_policy_document" "github_assume" {
       test     = "ForAllValues:StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       # TODO: Replace <username> and <repository> with your GitHub username and repository name
-      values   = ["repo:/CarlOfHoly/smestadcom:*"]
+      values   = ["repo:CarlOfHoly/smestadcom:*"]
     }
     condition {
       test     = "ForAllValues:StringLike"
