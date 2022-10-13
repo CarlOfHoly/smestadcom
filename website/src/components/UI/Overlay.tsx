9b6a8c54fedda82ff0faa31a253a4567194982d0
Carl Smestad <carl.smestad@gmail.com>
2022-10-13 17:27:25 +0200

    remove current frontend

--- website/src/components/UI/Overlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import "../../../css/components/UI/Overlay.css"
-
-const Overlay = () => <div className="overlay"></div>
-
-export default Overlay
--- website/src/components/Utils/Debug.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Debug: React.FC = () => {
-  return <div></div>
-}
-
-export default Debug
